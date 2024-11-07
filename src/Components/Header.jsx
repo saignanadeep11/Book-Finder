f5b2df367bf00b6f826de62d984083dc91b3e5bf
saignanadeep11 <saignanadeep11@gmail.com>
2024-11-07 16:20:05 +0530

    first comit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import "./styles/header.css";
+function Header() {
+  return (
+    <>
+      <div className="nameDiv">
+        <h1>BOOK FINDER</h1>
+      </div>
+    </>
+  );
+}
+export default Header;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,18 @@
+import "./styles/home.css";
+
+function Home() {
+  return (
+    <>
+      <div className="homeMainDiv">
+        <div></div>
+        <div className="inHomeMainDiv">
+          <input type="text" />
+          <div className="searchDiv">S</div>
+        </div>
+        <div></div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
